565dde56fd7baf959247228d2948fe48e5c1ec19
SernikDev <ziec.lukasz@gmail.com>
2018-02-04 22:01:15 +0100

    Add: Page info model

--- models/page_model.php
+++ models/page_model.php
@@ -6,7 +6,13 @@ class Page_Model extends Model {
         parent::__construct();
         $this->db = new Database();
     }
-    
 
-    
+    public function pageInfo($id) {
+        return $this->db->select("select A.`page_name`,A.`page_content`,A.`page_create_date`,A.`page_seo_title`,A.`page_seo_description`,B.`fname`,B.`lname`
+FROM `page` A
+LEFT JOIN `user` B ON A.`page_author_id` = B.`id`
+WHERE
+A.`page_id` = {$id};");
+    }
+
 }
